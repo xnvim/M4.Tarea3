b7ab7962dd4adf31b8fcf3a17fb4e3e901eb21da
efgc-clases <eguerra.cursos@gmail.com>
2022-12-15 17:41:27 -0600

    Se agrego codigo base para la tarea

---
+++ script/script.js
@@ -0,0 +1,57 @@
+//Arreglo con paises iniciales
+const paises = [
+    {
+        nombre: "Guatemala",
+        bandera: "https://bit.ly/3Bwr2rm",
+        capital: "Ciudad de Guatemala",
+        idioma_oficial: "Español",
+        moneda: "Quetzal"
+    },
+    {
+        nombre: "Argentina",
+        bandera: "https://bit.ly/3BEJC0y",
+        capital: "Buenos Aires",
+        idioma_oficial: "Español",
+        moneda: "Peso"
+    },
+    {
+        nombre: "Francia",
+        bandera: "https://bit.ly/3HFZ7t5",
+        capital: "Paris",
+        idioma_oficial: "Frances",
+        moneda: "Euro"
+    },
+    {
+        nombre: "El Salvador",
+        bandera: "https://bit.ly/3uRN5F6",
+        capital: "San Salvador",
+        idioma_oficial: "Español",
+        moneda: "Dolar"
+    },
+    {
+        nombre: "Honduras",
+        bandera: "https://bit.ly/3UWcrMM",
+        capital: "Tegucigalpa",
+        idioma_oficial: "Español",
+        moneda: "Lempira"
+    }
+];
+
+
+mostrarPaises();
+agregarMonitores();
+
+//funcion para agregar listeners a los botones
+function agregarMonitores() {
+    //TODO
+}
+
+
+//funcion que mostrara las paises en la pagina
+function mostrarPaises() {
+  //TODO
+}
+
+//funcion que permite agregar un pais al arreglo
+function agregarPais() {
+}
