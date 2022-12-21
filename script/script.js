4d0b4740abb88f3fb8bf8bf3cadb8d4c40210b8e
xnvim <josuejavierverbo@gmail.com>
2022-12-21 11:11:28 -0600

    M4.T3 - 3C Función agregarMonitores implementada

--- script/script.js
+++ script/script.js
@@ -1,53 +1,53 @@
 //Arreglo con paises iniciales
 const paises = [
-    {
-        nombre: "Guatemala",
-        bandera: "https://bit.ly/3Bwr2rm",
-        capital: "Ciudad de Guatemala",
-        idioma_oficial: "Español",
-        moneda: "Quetzal"
-    },
-    {
-        nombre: "Argentina",
-        bandera: "https://bit.ly/3BEJC0y",
-        capital: "Buenos Aires",
-        idioma_oficial: "Español",
-        moneda: "Peso"
-    },
-    {
-        nombre: "Francia",
-        bandera: "https://bit.ly/3HFZ7t5",
-        capital: "Paris",
-        idioma_oficial: "Frances",
-        moneda: "Euro"
-    },
-    {
-        nombre: "El Salvador",
-        bandera: "https://bit.ly/3uRN5F6",
-        capital: "San Salvador",
-        idioma_oficial: "Español",
-        moneda: "Dolar"
-    },
-    {
-        nombre: "Honduras",
-        bandera: "https://bit.ly/3UWcrMM",
-        capital: "Tegucigalpa",
-        idioma_oficial: "Español",
-        moneda: "Lempira"
-    }
+  {
+    nombre: "Guatemala",
+    bandera: "https://bit.ly/3Bwr2rm",
+    capital: "Ciudad de Guatemala",
+    idioma_oficial: "Español",
+    moneda: "Quetzal"
+  },
+  {
+    nombre: "Argentina",
+    bandera: "https://bit.ly/3BEJC0y",
+    capital: "Buenos Aires",
+    idioma_oficial: "Español",
+    moneda: "Peso"
+  },
+  {
+    nombre: "Francia",
+    bandera: "https://bit.ly/3HFZ7t5",
+    capital: "Paris",
+    idioma_oficial: "Frances",
+    moneda: "Euro"
+  },
+  {
+    nombre: "El Salvador",
+    bandera: "https://bit.ly/3uRN5F6",
+    capital: "San Salvador",
+    idioma_oficial: "Español",
+    moneda: "Dolar"
+  },
+  {
+    nombre: "Honduras",
+    bandera: "https://bit.ly/3UWcrMM",
+    capital: "Tegucigalpa",
+    idioma_oficial: "Español",
+    moneda: "Lempira"
+  }
 ];
 
 agregarMonitores();
 
 //funcion para agregar listeners a los botones
 function agregarMonitores() {
-    //TODO
+  document.getElementById('mostrarPaises').addEventListener('click', mostrarPaises);
+  document.getElementById('agregarPais').addEventListener('click', agregarPais);
 }
 
 
 //funcion que mostrara las paises en la pagina
 function mostrarPaises() {
-  //TODO
 }
 
 //funcion que permite agregar un pais al arreglo
